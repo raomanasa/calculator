8dd05097c87286847255ee859e64b7a97faf065b
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-03-09 21:47:37 +0100

    adds css

--- src/App.jsx
+++ src/App.jsx
@@ -53,23 +53,23 @@ class App extends Component{
   }
   render(){
   return (
-   <div>
-     <button onClick={() => this.handleClick(1)}>1</button>
-     <button onClick={() => this.handleClick(2)}>2</button>
-     <button onClick={() => this.handleClick(3)}>3</button><br/>
-     <button onClick={() => this.handleClick(4)}>4</button>
-     <button onClick={() => this.handleClick(5)}>5</button>
-     <button onClick={() => this.handleClick(6)}>6</button><br/>
-     <button onClick={() => this.handleClick(7)}>7</button>
-     <button onClick={() => this.handleClick(8)}>8</button>
-     <button onClick={() => this.handleClick(9)}>9</button><br/>
-     <button onClick={() => this.handleClick(0)}>0</button>
-     <button onClick={() => this.handleClick("+")}>+</button>
-     <button onClick={() => this.handleClick("-")}>-</button><br/>
-     <button onClick={() => this.handleClick("%")}>%</button>
-     <button onClick={() => this.handleClick("*")}>*</button>
-     <button onClick={() => this.handleResult()}>=</button>
-     <button onClick={() => this.handleClear()}>CLEAR</button>
+   <div className="box">
+     <button className="button" onClick={() => this.handleClick(1)}>1</button>
+     <button className="button" onClick={() => this.handleClick(2)}>2</button>
+     <button className="button" onClick={() => this.handleClick(3)}>3</button><br/>
+     <button className="button" onClick={() => this.handleClick(4)}>4</button>
+     <button className="button" onClick={() => this.handleClick(5)}>5</button>
+     <button className="button" onClick={() => this.handleClick(6)}>6</button><br/>
+     <button className="button" onClick={() => this.handleClick(7)}>7</button>
+     <button className="button" onClick={() => this.handleClick(8)}>8</button>
+     <button className="button" onClick={() => this.handleClick(9)}>9</button><br/>
+     <button className="button" onClick={() => this.handleClick(0)}>0</button>
+     <button className="button" onClick={() => this.handleClick("+")}>+</button>
+     <button className="button" onClick={() => this.handleClick("-")}>-</button><br/>
+     <button className="button" onClick={() => this.handleClick("%")}>%</button>
+     <button className="button" onClick={() => this.handleClick("*")}>*</button>
+     <button className="button" onClick={() => this.handleResult()}>=</button><br/>
+     <button className="button" onClick={() => this.handleClear()}>CLEAR</button>
      <p>{this.state.result}</p>
    </div>
   );
