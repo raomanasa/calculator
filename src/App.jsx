08b556bce3c88e2b3e1bb992ab0161ea21dc8cc4
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-03-09 12:22:50 +0100

    adds logic to store numbers and signs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
+import { throwStatement } from '@babel/types';
 
 class App extends Component{
+  constructor (){
+    super()
+    this.state = {
+      number: [],
+      result: null
+    }
+  }
+  handleClick (num){
+    let temp = this.state.number
+    console.log(temp)
+    temp.push(num)
+    this.setState({number:temp})
+    console.log(temp)
+
+  }
   render(){
   return (
    <div>
-     <button>1</button>
-     <button>2</button>
-     <button>3</button><br/>
-     <button>4</button>
-     <button>5</button>
-     <button>6</button><br/>
-     <button>7</button>
-     <button>8</button>
-     <button>9</button><br/>
-     <button>0</button>
-     <button>+</button>
-     <button>-</button><br/>
-     <button>%</button>
-     <button>*</button>
+     <button onClick={() => this.handleClick(1)}>1</button>
+     <button onClick={() => this.handleClick(2)}>2</button>
+     <button onClick={() => this.handleClick(3)}>3</button><br/>
+     <button onClick={() => this.handleClick(4)}>4</button>
+     <button onClick={() => this.handleClick(5)}>5</button>
+     <button onClick={() => this.handleClick(6)}>6</button><br/>
+     <button onClick={() => this.handleClick(7)}>7</button>
+     <button onClick={() => this.handleClick(8)}>8</button>
+     <button onClick={() => this.handleClick(9)}>9</button><br/>
+     <button onClick={() => this.handleClick(0)}>0</button>
+     <button onClick={() => this.handleClick("+")}>+</button>
+     <button onClick={() => this.handleClick("-")}>-</button><br/>
+     <button onClick={() => this.handleClick("%")}>%</button>
+     <button onClick={() => this.handleClick("*")}>*</button>
      <button>=</button>
    </div>
   );
