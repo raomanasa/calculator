c5ff81b8d7cda47f1632e9b4ccd44b2ab8f22ebd
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-03-09 10:47:56 +0100

    adds buttons

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render(){
+  return (
+   <div>
+     <button>1</button>
+     <button>2</button>
+     <button>3</button><br/>
+     <button>4</button>
+     <button>5</button>
+     <button>6</button><br/>
+     <button>7</button>
+     <button>8</button>
+     <button>9</button><br/>
+     <button>0</button>
+     <button>+</button>
+     <button>-</button><br/>
+     <button>%</button>
+     <button>*</button>
+     <button>=</button>
+   </div>
+  );
+  }
+}
+
+export default App;
