3ea26707fb0158c2bc40a53a72804e35eb5fc69b
drmanasa18.rao@gmail.com <drmanasa18.rao@gmail.com>
2020-05-18 13:12:19 +0200

    adds logic for multiple digit function

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,64 @@ import React, { Component } from 'react';
 import { throwStatement } from '@babel/types';
 
 class App extends Component{
-  constructor (){
-    super()
-    this.state = {
-      data: [],
-      result: null
+    constructor() {
+      super();
+      this.state = { array: [], result: null };
+      this.handleClick = this.handleClick.bind(this);
+      this.handleResult = this.handleResult.bind(this);
+      this.handleClear = this.handleClear.bind(this);
     }
-  }
-  handleClear (){
-    this.setState({data: [], result: null})
-  }
-  handleResult (){
-    let number = []
-    let sign = []
-    for (let i=0;i<this.state.data.length;i++) {
-      if (typeof this.state.data[i] == 'string') {
-        sign.push(this.state.data[i])
-      }
-      else {
-        number.push(this.state.data[i])
-      }
+    handleClick(str) {
+      let ary = this.state.array;
+      ary.push(str);
+      this.setState({ array: ary });
     }
-    let ans = number[0]
-    for(let i=0;i<sign.length;i++){
-      if (sign[i]==="+") {
-        ans=ans+number[i+1]
-      }
-      else if (sign[i]==="-"){
-        ans=ans-number[i+1]
-      }
-      else if (sign[i]==="%"){
-        ans=ans%number[i+1]
-      }
-      else if (sign[i]==="*"){
-        ans=ans*number[i+1]
-      }
+    handleClear() {
+      this.setState({ array: [], result: null })  
     }
-    this.setState({result:ans})
-    console.log(number)
-    console.log(sign)
-    console.log(ans)
-  }
-  handleClick (num){
-    let temp = this.state.data
-    console.log(temp)
-    temp.push(num)
-    this.setState({data:temp})
-    console.log(temp)
-
-  }
+    handleResult() {
+      let num = [];
+      let num2 = [];
+      let temp = [];
+      let sign = [];
+  
+      let count = 0;
+      for (let i = 0; i < this.state.array.length; i++) {
+        if (typeof this.state.array[i] === "string") {
+          sign.push(this.state.array[i]);
+          num2.push(temp);
+          temp = [];
+        } else {
+          temp.push(this.state.array[i]); 
+        }
+      }
+      num2.push(temp);
+      let temp2;
+      for (let i = 0; i < num2.length; i++) {
+        count = 0;
+        temp2 = 0;
+        for(let j = num2[i].length-1 ; j >= 0; j--) {
+          temp2 = temp2 + num2[i][j]* Math.pow(10,count);
+          count ++;
+        }
+        num.push(temp2)
+      }
+      let result = num[0];
+      for (let i = 0; i < sign.length; i++) {
+        if (sign[i] === "+") {
+          result = result + num[i + 1];
+        } else if (sign[i] === "-") {
+          result = result - num[i + 1];
+        } else if (sign[i] === "*") {
+          result = result * num[i + 1];
+        } else if (sign[i] === "/") {
+          result = result / num[i + 1];
+        } else if (sign[i] === "%") {
+          result = result % num[i + 1];
+        }
+      }
+      this.setState({ result: result });
+    }
   render(){
   return (
     <div className="box">
